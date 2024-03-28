96017a73acd582a54a3ea440f5457fc453758ff2
Vladyslav Korniienko <maradonna624@gmail.com>
2024-03-28 20:37:23 +0200

    Add unique constraints for properties in User and Product models

--- StoreProject.DAL/Context/StoreContext.cs
+++ StoreProject.DAL/Context/StoreContext.cs
@@ -18,15 +18,32 @@ namespace StoreProject.DAL.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            //Product config
             modelBuilder.Entity<Product>()
                 .Property(p => p.PriceUSD)
                 .HasColumnType("decimal(18,2)");
+            modelBuilder.Entity<Product>()
+                .HasIndex(p => p.Name)
+                .IsUnique();
+
+            //User config
             modelBuilder.Entity<User>()
                 .Property(u => u.Balance)
                 .HasColumnType("decimal(18,2)");
             modelBuilder.Entity<User>()
                 .Property(u => u.Username)
                 .IsRequired();
+            modelBuilder.Entity<User>()
+                .Property(u => u.Password)
+                .IsRequired();
+            modelBuilder.Entity<User>()
+                .Property(u => u.Email)
+                .IsRequired();
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+            
         }
     }
 }
