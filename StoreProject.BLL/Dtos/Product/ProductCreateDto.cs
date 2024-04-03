6c15bcd8ad44281e57b8edbc9c8371d098e5ad4f
Vladyslav Korniienko <maradonna624@gmail.com>
2024-04-03 19:11:44 +0300

    Change prop names in ProductCreateDto

--- StoreProject.BLL/Dtos/Product/ProductCreateDto.cs
+++ StoreProject.BLL/Dtos/Product/ProductCreateDto.cs
@@ -7,12 +7,13 @@ using System.Threading.Tasks;
 
 namespace StoreProject.BLL.Dtos.Product
 {
+    //Do I even need it?
     public class ProductCreateDto
     {
         public string? Name { get; set; }
         public decimal PriceUSD { get; set; }
-        public string? Genre { get; set; }
-        public string? Category { get; set; }
+        public string? GenreName { get; set; }
+        public string? CategoryName { get; set; }
         public string? Description { get; set; }
     }
 }
