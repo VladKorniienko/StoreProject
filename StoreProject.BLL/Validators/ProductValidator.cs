dff34a5ca2657d104ec52ba2c0836f7b93107f9a
Vladyslav Korniienko <maradonna624@gmail.com>
2024-04-07 17:29:03 +0300

    Add additional validation rules for Product

--- StoreProject.BLL/Validators/ProductValidator.cs
+++ StoreProject.BLL/Validators/ProductValidator.cs
@@ -1,15 +1,30 @@
 ﻿using FluentValidation;
-using StoreProject.DAL.Models;
+using StoreProject.BLL.Dtos.Product;
 
 
 namespace StoreProject.BLL.Validators
 {
-    public class ProductValidator : AbstractValidator<Product>
+    public class ProductValidator : AbstractValidator<ProductCreateOrUpdateDto>
     {
         public ProductValidator() 
         {
-            RuleFor(product => product.Name).NotEmpty().WithMessage("Product name is required.");
-            //RuleFor(product => product.PriceUSD).GreaterThanOrEqualTo(0).W
+            RuleFor(product => product.Name)
+                .NotEmpty().WithMessage("Product name is required.")
+                .Length(1, 100).WithMessage("Product name must be between 1 and 100 characters.");
+
+            RuleFor(product => product.PriceUSD)
+             .GreaterThanOrEqualTo(0).WithMessage("Price cannot be negative.")
+             .LessThanOrEqualTo(10000).WithMessage("Price cannot exceed 10000.");
+
+            RuleFor(product => product.GenreId)
+            .NotNull().WithMessage("GenreId is required.");
+
+            RuleFor(product => product.CategoryId)
+                .NotNull().WithMessage("CategoryId is required.");
+
+            RuleFor(product => product.Description)
+            .MaximumLength(500).WithMessage("Description cannot exceed 500 characters.");
+
         }
     }
 }
