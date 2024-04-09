cb33e770024f7b7131b8153f6b955377fac76999
Vladyslav Korniienko <maradonna624@gmail.com>
2024-04-09 15:52:46 +0300

    Configure dependencies

--- StoreProject.BLL/ServiceExtensions.cs
+++ StoreProject.BLL/ServiceExtensions.cs
@@ -6,20 +6,25 @@ using FluentValidation;
 using Microsoft.AspNetCore.Identity;
 using StoreProject.DAL.Models;
 using StoreProject.DAL.Context;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace StoreProject.BLL
 {
     public static class ServiceExtensions
     {
-        public static void ConfigureBLL(this IServiceCollection services)
+        public static void ConfigureBLL(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddScoped<IUserService, UserService>();
-            services.AddScoped<IProductService, ProductService>();
-            services.AddScoped<IGenreService, GenreService>();
-            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IProductService, ProductService>();
+            services.AddTransient<IGenreService, GenreService>();
+            services.AddTransient<ICategoryService, CategoryService>();
+            services.AddTransient<IAuthService, AuthService>();
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-            services.AddScoped<UserManager<User>>();
+            services.AddTransient<UserManager<User>>();
+
             services.AddIdentityCore<User>(options =>
             {
                 options.Password.RequiredLength = 6;
@@ -28,8 +33,19 @@ namespace StoreProject.BLL
                 options.Password.RequireUppercase = false;
                 options.Password.RequireNonAlphanumeric = false;
                 options.User.RequireUniqueEmail = true;
-            }).AddEntityFrameworkStores<StoreContext>();
+                options.SignIn.RequireConfirmedAccount = false;
+            }).AddRoles<IdentityRole>()
+              .AddRoleManager<RoleManager<IdentityRole>>()
+              .AddRoleStore<RoleStore<IdentityRole, StoreContext>>()
+              .AddTokenProvider<DataProtectorTokenProvider<User>>("REFRESHTOKENPROVIDER")
+              .AddEntityFrameworkStores<StoreContext>();
+            //all three AddRoles,AddRoleManager,AddRoleStore are required for the app to function if
+            //AddIdentityCore is used instead of AddIdentity
 
+            services.Configure<DataProtectionTokenProviderOptions>(options =>
+            {
+                options.TokenLifespan = TimeSpan.FromSeconds(Convert.ToDouble(configuration["JwtSettings:RefreshTokenExpireTimeSeconds"]));
+            });
 
         }
     }
--- StoreProject/Program.cs
+++ StoreProject/Program.cs
@@ -1,30 +1,81 @@
 using StoreProject.DAL;
 using StoreProject.BLL;
 using StoreProject.ExceptionHandler;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using StoreProject.BLL.Dtos.Token;
+using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 //so-called "composition root"
+
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
 builder.Services.ConfigureDAL(builder.Configuration);
-builder.Services.ConfigureBLL();
+builder.Services.ConfigureBLL(builder.Configuration);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecurityKey"]))
+    };
+});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Test API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
+
 
 var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseHttpsRedirection();
 app.UseCors(x => x
             .AllowAnyOrigin()
             .AllowAnyMethod()
             .AllowAnyHeader());
-app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
 app.Run();
